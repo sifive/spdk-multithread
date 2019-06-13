e57cb144b0335b881376a45b0405eb812a604d81
Haotian Wang <haotian.wang@sifive.com>
2019-06-12 17:22:38 -0700

    Program now printed two statements in two threads
    No mutex lock is used between the two threads. Each thread has its own
    queue pair. The two queue pairs interact with the same namespace. Each
    thread goes through the whole write and read process.

--- spdk-multithread.c
+++ spdk-multithread.c
@@ -1,5 +1,4 @@
 #include "spdk/stdinc.h"
-
 #include "spdk/nvme.h"
 #include "spdk/env.h"
 
@@ -55,10 +54,13 @@ struct task {
     bool is_completed;
     bool is_on_cmb;
     struct spdk_nvme_qpair *qpair;
+    const char *target;
+    size_t length;
 };
 
 /* String to be written to nvme and read back to stdout */
 static const char str[] = "Hello World!";
+static const char str_back[] = "Bye World!";
 
 /* Callback function on read complete */
 static void read_complete(void *cb_ctx, const struct spdk_nvme_cpl *cpl_ctx) {
@@ -69,7 +71,7 @@ static void read_complete(void *cb_ctx, const struct spdk_nvme_cpl *cpl_ctx) {
 
     /* Free the memory allocated for the read buffer */
     if (iotask->is_on_cmb) {
-        spdk_nvme_ctrlr_free_cmb_io_buffer(iotask->ctrlr, iotask->buffer, sizeof(iotask->buffer));
+        spdk_nvme_ctrlr_free_cmb_io_buffer(iotask->ctrlr, iotask->buffer, iotask->length);
     } else {
         spdk_free(iotask->buffer);
     }
@@ -84,14 +86,14 @@ static void write_complete(void *cb_ctx, const struct spdk_nvme_cpl *cpl_ctx) {
     
     /* Free the buffer allocated to the write command */
     if (iotask->is_on_cmb) {
-        spdk_nvme_ctrlr_free_cmb_io_buffer(iotask->ctrlr, iotask->buffer, sizeof(iotask->buffer));
+        spdk_nvme_ctrlr_free_cmb_io_buffer(iotask->ctrlr, iotask->buffer, iotask->length);
     } else {
         spdk_free(iotask->buffer);
     }
 
     /* Allocate read buffer on either host or cmb */
-    if ((iotask->buffer = spdk_nvme_ctrlr_alloc_cmb_io_buffer(iotask->ctrlr, sizeof(str))) == NULL) {
-        iotask->buffer = spdk_malloc(sizeof(str), 0, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
+    if ((iotask->buffer = spdk_nvme_ctrlr_alloc_cmb_io_buffer(iotask->ctrlr, iotask->length)) == NULL) {
+        iotask->buffer = spdk_malloc(iotask->length, 0, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
         iotask->is_on_cmb = false;
     } else {
         iotask->is_on_cmb = true;
@@ -118,6 +120,42 @@ static void cleanup(void) {
     }
 }
 
+/* The thread pool for the program */
+static pthread_t thread_pool[2];
+
+/* Specify the routine in each thread */
+static void *thread_init(void *arg) {
+    struct task *iotask = arg;
+
+    /* Allocate queue pair for this thread */
+    struct spdk_nvme_qpair *qpair = spdk_nvme_ctrlr_alloc_io_qpair(iotask->ctrlr, NULL, 0);
+    iotask->qpair = qpair;
+
+    /* Initialize the task for this namespace */
+    if ((iotask->buffer = spdk_nvme_ctrlr_alloc_cmb_io_buffer(iotask->ctrlr, iotask->length)) == NULL) {
+        /* cannot allocate memory on cmb */
+        iotask->is_on_cmb = false;
+        /* Allocate on host memory */
+        iotask->buffer = spdk_zmalloc(iotask->length, 0, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
+    } else {
+        iotask->is_on_cmb = true;
+    }
+    memcpy(iotask->buffer, iotask->target, iotask->length);
+
+    /* Submit write command to queue */
+    if (spdk_nvme_ns_cmd_write(iotask->ns, qpair, iotask->buffer, 0, 1, write_complete, iotask, 0)) {
+        fprintf(stderr, "Write command cannot be submitted");
+        exit(1);
+    }
+
+    /* Poll for completitions on this qpair until the io routine is completed */
+    while (iotask->is_completed == false) {
+        spdk_nvme_qpair_process_completions(qpair, 0);
+    }
+
+    return NULL;
+}
+
 int main(int argc, char **argv) {
     spdk_env_opts_init(&opts);
     if(spdk_env_init(&opts)) {
@@ -133,44 +171,35 @@ int main(int argc, char **argv) {
     }
 
     /* For each active namespace in the singly list, print hello world */
+    struct task iotask1;
+    struct task iotask2;
+    iotask1.is_completed = false;
+    iotask2.is_completed = false;
+    iotask1.length = sizeof(str);
+    iotask2.length = sizeof(str_back);
+    iotask1.target = str;
+    iotask2.target = str_back;
+
     head = n_list;
     while (n_list != NULL) {
-        struct task iotask;
-        iotask.is_completed = false;
-        iotask.ns = n_list->current;
-
+        iotask1.ns = n_list->current;
+        iotask2.ns = n_list->current;
+        iotask1.ctrlr = n_list->ctrlr;
+        iotask2.ctrlr = n_list->ctrlr;
+        
         /* Print namespace size, namespace capacity and formatted lba size */
         const struct spdk_nvme_ns_data *ns_data = spdk_nvme_ns_get_data(n_list->current);
         printf("Namespace size is %ld\n", ns_data->nsze);
         printf("Namespace capacity is %ld\n", ns_data->ncap);
         printf("Namespace sector size is %u\n", spdk_nvme_ns_get_sector_size(n_list->current));
 
-        /* Allocate queue pair for this thread */
-        struct spdk_nvme_qpair *qpair = spdk_nvme_ctrlr_alloc_io_qpair(n_list->ctrlr, NULL, 0);
-        iotask.qpair = qpair;
+        /* Created two threads */
+        pthread_create(thread_pool, NULL, thread_init, &iotask1);
+        pthread_create(thread_pool + 1, NULL, thread_init, &iotask2);
 
-        /* Initialize the task for this namespace */
-        iotask.ctrlr = n_list->ctrlr;
-        if ((iotask.buffer = spdk_nvme_ctrlr_alloc_cmb_io_buffer(iotask.ctrlr, sizeof(str))) == NULL) {
-            /* cannot allocate memory on cmb */
-            iotask.is_on_cmb = false;
-            /* Allocate on host memory */
-            iotask.buffer = spdk_zmalloc(sizeof(str), 0, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
-        } else {
-            iotask.is_on_cmb = true;
-        }
-        memcpy(iotask.buffer, str, sizeof(str));
-
-        /* Submit write command to queue */
-        if (spdk_nvme_ns_cmd_write(n_list->current, qpair, iotask.buffer, 0, 1, write_complete, &iotask, 0)) {
-            fprintf(stderr, "Write command cannot be submitted");
-            return 1;
-        }
-
-        /* Poll for completitions on this qpair until the io routine is completed */
-        while (iotask.is_completed == false) {
-            spdk_nvme_qpair_process_completions(qpair, 0);
-        }
+        /* Wait for threads to join */
+        pthread_join(thread_pool[0], NULL);
+        pthread_join(thread_pool[1], NULL);
 
         /* Proceed to process the next namespace */
         n_list = n_list->next;
