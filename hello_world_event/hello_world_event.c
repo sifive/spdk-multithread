7d249e65f3e2057b3434160988e9e734b9dab0f1
Haotian Wang <haotian.wang@sifive.com>
2019-06-14 16:21:20 -0700

    Use event framework to achieve true multithread
    hello_world_event now uses true multithreading in the sense that each
    reactor executes its event queues separately. The first thread collects
    the return information of other threads and terminates the program once
    it decides the process is done.

--- hello_world_event/hello_world_event.c
+++ hello_world_event/hello_world_event.c
@@ -5,6 +5,9 @@
 /* This program demonstrates the multithreaded hello world 
  * using the event framework provided in the SPDK library */
 
+static int rc_count = 0;
+
+/* Main context struct to pass to different callback functions */
 struct io_ctx {
     struct spdk_nvme_qpair *qpair;
     bool is_on_cmb;
@@ -12,13 +15,16 @@ struct io_ctx {
     size_t load;
     uint64_t starting_lba;
     bool is_complete;
+    int core_idx;
+    char *msg;
 };
 
-static char msg1[] = "Event framework does the I/O One!";
-static size_t load1 = sizeof(msg1);
-
-static char msg2[] = "Event framework does the I/O Two!";
-static size_t load2 = sizeof(msg2);
+/* Initalize I/O messages */
+static char msg2[] = "Core 1 reporting!";
+static char msg3[] = "Core 2 reporting!";
+static char msg4[] = "Core 3 reporting!";
+static char *msgs[3] = {msg2, msg3, msg4};
+static size_t loads[3] = {sizeof(msg2), sizeof(msg3), sizeof(msg4)};
 
 static struct spdk_app_opts app_opts;
 
@@ -69,21 +75,30 @@ static void ctrlr_ns_init(void) {
     }
 }
 
-/* Callback on successful read */
-static void read_complete(void *arg, const struct spdk_nvme_cpl *cpl) {
-    struct io_ctx *ctx = arg;
+/* Threads report complete status to thread 0 */
+static void report_complete(void *arg1, void *arg2) {
+    struct io_ctx *ctx = arg1;
 
-    /* Print information to standard out */
-    printf("%s is read from sector %ld\n", (char *) ctx->buffer, ctx->starting_lba);
+    printf("%s\n", (char *) ctx->buffer);
 
     /* Deallocate read buffer */
     if (ctx->is_on_cmb) {
-        spdk_nvme_ctrlr_free_cmb_io_buffer(nvme_ctrlr, ctx->buffer, load1);
-        printf("Read buffer on cmb is deallocated\n");
+        spdk_nvme_ctrlr_free_cmb_io_buffer(nvme_ctrlr, ctx->buffer, ctx->load);
+        printf("Read buffer for core %d on cmb is deallocated\n", ctx->core_idx);
     } else {
         spdk_free(ctx->buffer);
-        printf("Read buffer on host memory is deallocated\n");
+        printf("Read buffer for core %d on host memory is deallocated\n", ctx->core_idx);
     }
+
+    rc_count++;
+    if (rc_count == 3) {
+        spdk_app_stop(0);
+    }
+}
+
+/* Callback on successful read */
+static void read_complete(void *arg, const struct spdk_nvme_cpl *cpl) {
+    struct io_ctx *ctx = arg;
 
     /* Mark task as complete */
     ctx->is_complete = true;
@@ -95,11 +110,11 @@ static void write_complete(void *arg, const struct spdk_nvme_cpl *cpl) {
 
     /* Deallocate write buffer */
     if (ctx->is_on_cmb) {
-        spdk_nvme_ctrlr_free_cmb_io_buffer(nvme_ctrlr, ctx->buffer, load1);
-        printf("Write buffer on cmb is deallocated\n");
+        spdk_nvme_ctrlr_free_cmb_io_buffer(nvme_ctrlr, ctx->buffer, ctx->load);
+        printf("Write buffer on cmb on core %d is deallocated\n", ctx->core_idx);
     } else {
         spdk_free(ctx->buffer);
-        printf("Write buffer on host memory is deallocated\n");
+        printf("Write buffer on host memory on core %d is deallocated\n", ctx->core_idx);
     }
 
     /* After write complete, do read */
@@ -107,10 +122,10 @@ static void write_complete(void *arg, const struct spdk_nvme_cpl *cpl) {
     void *read_buffer = NULL;
     if ((read_buffer = spdk_nvme_ctrlr_alloc_cmb_io_buffer(nvme_ctrlr, ctx->load))) {
         ctx->is_on_cmb = true;
-        printf("Read buffer is now on cmb\n");
+        printf("Read buffer for core %d is now on cmb\n", ctx->core_idx);
     } else {
         read_buffer = spdk_malloc(ctx->load, 0, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
-        printf("Read buffer is on host memory\n");
+        printf("Read buffer for core %d is on host memory\n", ctx->core_idx);
     }
     ctx->buffer = read_buffer;
 
@@ -123,85 +138,75 @@ static void write_complete(void *arg, const struct spdk_nvme_cpl *cpl) {
 
 /* Event function that prints a message */
 static void print_msg(void *arg1, void *arg2) {
+    struct io_ctx *ctx = (struct io_ctx *) arg1;
 
     /* Allocate queue pair */
     struct spdk_nvme_qpair *qpair = 
         spdk_nvme_ctrlr_alloc_io_qpair(nvme_ctrlr, NULL, 0);
+    ctx->qpair = qpair;
 
     /* Allcoate buffer on CMB or host for write */
-    bool is_on_cmb1 = false;
-    void *write_buffer1 = NULL;
-    if ((write_buffer1 = spdk_nvme_ctrlr_alloc_cmb_io_buffer(nvme_ctrlr, load1))) {
-        is_on_cmb1 = true;
-        printf("Write buffer 1 is on cmb\n");
+    void *write_buffer = NULL;
+    if ((write_buffer = spdk_nvme_ctrlr_alloc_cmb_io_buffer(nvme_ctrlr, ctx->load))) {
+        ctx->is_on_cmb = true;
+        printf("Write buffer for core %d is on cmb\n", ctx->core_idx);
     } else {
-        write_buffer1 = spdk_malloc(load1, 0, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
-        printf("Write buffer 1 is on host memory\n");
+        write_buffer = spdk_malloc(ctx->load, 0, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
+        printf("Write buffer for core %d is on host memory\n", ctx->core_idx);
     }
-    bool is_on_cmb2 = false;
-    void *write_buffer2 = NULL;
-    if ((write_buffer2 = spdk_nvme_ctrlr_alloc_cmb_io_buffer(nvme_ctrlr, load2))) {
-        is_on_cmb2 = true;
-        printf("Write buffer 2 is on cmb\n");
-    } else {
-        write_buffer2 = spdk_malloc(load2, 0, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
-        printf("Write buffer 2 is on host memory\n");
-    }
+    ctx->buffer = write_buffer;
 
     /* Write message to the allocated memory buffer */
-    memcpy(write_buffer1, msg1, load1);
-    memcpy(write_buffer2, msg2, load2);
+    memcpy(write_buffer, ctx->msg, ctx->load);
 
     /* Submit write command */
-    struct io_ctx ctx1 = {
-        .qpair = qpair,
-        .is_on_cmb = is_on_cmb1,
-        .buffer = write_buffer1,
-        .load = load1,
-        .starting_lba = 0,
-        .is_complete = false
-    };
-    if (spdk_nvme_ns_cmd_write(nvme_ns, qpair, write_buffer1, 0, 1, write_complete,
-                &ctx1, 0)) {
-        fprintf(stderr, "Failure in submitting write command 1\n");
+    if (spdk_nvme_ns_cmd_write(nvme_ns, qpair, write_buffer, ctx->starting_lba, 
+                1, write_complete, ctx, 0)) {
+        fprintf(stderr, "Failure in submitting write command on core %d\n", ctx->core_idx);
     }
-    printf("Submitted write command 1 to sector 0\n");
-    struct io_ctx ctx2 = {
-        .qpair = qpair,
-        .is_on_cmb = is_on_cmb2,
-        .buffer = write_buffer2,
-        .load = load2,
-        .starting_lba = 1,
-        .is_complete = false
-    };
-    if (spdk_nvme_ns_cmd_write(nvme_ns, qpair, write_buffer2, 1, 1, write_complete,
-                &ctx2, 0)) {
-        fprintf(stderr, "Failure in submitting write command 2\n");
-    }
-    printf("Submitted write command 2 to sector 1\n");
+    printf("Submitted write command on core %d to sector %ld\n", ctx->core_idx, ctx->starting_lba);
 
     /* Continous process qpair until the two jobs are complete */
-    while (!ctx1.is_complete || !ctx2.is_complete) {
+    while (!ctx->is_complete) {
         if (spdk_nvme_qpair_process_completions(qpair, 0) < 0) {
-            fprintf(stderr, "Error occurred in processing queue pairs");
+            fprintf(stderr, "Error occurred in processing queue pairs on core %d", ctx->core_idx);
         }
     }
+
+    /* Notify thread 0 of completition */
+    struct spdk_event *event = spdk_event_allocate(0, report_complete, ctx, NULL);
+    spdk_event_call(event);
 }
 
+/* Prepare tasks for each reactor */
+static struct io_ctx ctxs[3];
+
+/* Prepare four events to the four reactors */
+static struct spdk_event *io_events[3];
+
 /* Main program after the hello is started */
-static void hello_start(void *ctx) {
+static void hello_start(void *arg) {
+    /* Intialize NMVe controller and namespace */
     ctrlr_ns_init();
-
-    /* On virtual mahcine dpdk can only detect one logical core,
-     * will send threads to work on this one lcore */
-    uint32_t first_core_id = spdk_env_get_first_core();
-    struct spdk_event *io_event = 
-        spdk_event_allocate(first_core_id, print_msg, NULL, NULL);
-    spdk_event_call(io_event);
-
-    /* Stop the app framework to unblock the app_start call. Let the 
-     * main function to continue executing */
-    spdk_app_stop(0);
+    ctxs[0].is_complete = false;
+    ctxs[0].starting_lba = 0;
+    ctxs[0].msg = msgs[0];
+    ctxs[0].is_on_cmb = false;
+    ctxs[0].load = loads[0];
+    ctxs[0].core_idx = spdk_env_get_next_core(spdk_env_get_first_core());
+    io_events[0] = spdk_event_allocate(ctxs[0].core_idx, print_msg, ctxs, NULL);
+    for (int i = 1; i < 3; i++) {
+        ctxs[i].is_complete = false;
+        ctxs[i].starting_lba = i;
+        ctxs[i].msg = msgs[i];
+        ctxs[i].is_on_cmb = false;
+        ctxs[i].load = loads[i];
+        ctxs[i].core_idx = spdk_env_get_next_core(ctxs[i - 1].core_idx);
+        io_events[i] = spdk_event_allocate(ctxs[i].core_idx, print_msg, ctxs + i, NULL);
+    }
+    for (int i = 0; i < 3; i++) {
+        spdk_event_call(io_events[i]);
+    }
 }
 
 /* cleanup to do after termination of app */
@@ -219,7 +224,10 @@ static void cleanup(void) {
 int main(int argc, char **argv) {
     /* Initialize the app event framework */
     spdk_app_opts_init(&app_opts);
-    app_opts.reactor_mask = "0x4";
+
+    /* Use all four cores, 0xF = 0b1111 */
+    app_opts.reactor_mask = "0xF";
+
     /* Start the event app framework */
     if (spdk_app_start(&app_opts, hello_start, NULL)) {
         fprintf(stderr, "Failed to start app framework\n");
@@ -227,5 +235,7 @@ int main(int argc, char **argv) {
     }
     printf("app program successfully terminated\n");
     cleanup();
+
+    spdk_app_fini();
     return 0;
 }
